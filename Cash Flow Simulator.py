4e2035cb96812d4b7889ba8474241cb2a4da2a93
James <73198564+x1-james@users.noreply.github.com>
2023-02-04 15:17:39 -0600

    Initial commit

---
+++ Cash Flow Simulator.py
@@ -0,0 +1,10 @@
+#
+# Cash Flow Simulator
+#
+# Basic cash flow simulator that shows projected cash flor for a home building business
+#
+
+# Import the necessary modules
+
+
+# Define the cash flow scenarios
